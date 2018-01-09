66350d25a1680ca94425ccbbea976ae55b7a341d
Erzan Rosbrianto <erzan81@gmail.com>
2018-01-09 14:06:44 +0700

    bug fixing monitoring metode

--- application/controllers/admin/Monitoring.php
+++ application/controllers/admin/Monitoring.php
@@ -24,12 +24,10 @@ class Monitoring extends CI_Controller {
 
         $this->load->model('MMatching');
 
-        //$p_id_upload = $this->input->post('p_id_upload');
-        $p_id_upload = "UPLOAD-20180109-000001";
-        $step_ke = 1;
-        //$step_ke = $this->input->post('step_ke');
+        $p_id_upload = $this->input->post('p_id_upload');
+        $step_ke = $this->input->post('step_ke');
         $detil_fix = array();
-        //$main = $this->MMatching->get_metode_pemadanan($p_id_upload);
+        
         $detil = $this->MMatching->get_metode_pemadanan_detil($p_id_upload);
             
             foreach ($detil as $det ) {
